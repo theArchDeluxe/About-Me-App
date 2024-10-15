6d53139ee64d077b45bc1937899af941a4f7d03c
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-10-15 07:10:13 +0300

    Initial Commit

---
+++ About Me App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me App
+//
+//  Created by Alexey Elfimov on 15.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
