1635d07e06fa2da8a4eb6bd080ffcff766cae965
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-10-16 16:11:22 +0300

    Fixed welcom view controller

--- About Me App/ViewController.swift
+++ About Me App/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let user = "User"
+    private let password = "Password"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = user
     }
-
-
+    
+    override func touchesBegan(_ touches: Set, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            
+        }
+        
+        
+    }
+    
 }
-
---
+++ About Me App/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  About Me App
+//
+//  Created by Alexey Elfimov on 16.10.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    var userName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
