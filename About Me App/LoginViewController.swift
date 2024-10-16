6fc030a7e905de36dbf96382a880a84e0dd61bc8
theArchDeluxe <156505949+theArchDeluxe@users.noreply.github.com>
2024-10-16 16:45:18 +0300

    Placed icons on welcome screen

--- About Me App/LoginViewController.swift
+++ About Me App/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
